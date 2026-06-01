c17f5313e2b06444315a2f5e6e93e717832dd3ed
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2026-06-01 13:42:16 +0530

    ui changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,6 +11,8 @@ import { GoSun } from "react-icons/go";
 import { FaRegMoon } from "react-icons/fa";
 import { useTheme } from "next-themes";
 import { Hanken_Grotesk } from "next/font/google";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const hanken = Hanken_Grotesk({
   subsets: ["latin"],
@@ -20,9 +22,16 @@ const hanken = Hanken_Grotesk({
 
 function Navbar() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
-    <div className={`${hanken.className} w-full flex items-center justify-center transition-all z-[130] duration-300 fixed top-0 left-0 right-0 backdrop-blur-lg`}>
+    <div
+      className={`${hanken.className} w-full flex items-center justify-center transition-all z-[130] duration-300 fixed top-0 left-0 right-0 backdrop-blur-lg`}
+    >
       <div className="w-full flex items-center justify-between px-4 sm:px-6 py-3 max-w-3xl gap-3">
         <div className="flex items-center gap-4 sm:gap-8 text-xs sm:text-sm text-neutral-400 font-semibold flex-wrap">
           <Link href={"/"}>Home</Link>
@@ -53,18 +62,38 @@ function Navbar() {
               <button
                 className="hover:bg-neutral-100 dark:hover:bg-neutral-900 duration-300 px-3 py-2 rounded"
                 onClick={() => {
-                  setTheme((prev)=>{
-                    return prev==="dark"?"light":"dark";
-                  })
+                  setTheme((prev) => {
+                    return prev === "dark" ? "light" : "dark";
+                  });
                 }}
               >
-                {theme==="dark"?<FaRegMoon/>:<GoSun/>}
+                <AnimatePresence mode="wait">
+                  {mounted && theme === "dark" ? (
+                    <motion.div
+                      key="moon"
+                      // initial={{ opacity: 1, filter: "blur(0px)", scale: 0.8 }}
+                      // animate={{ opacity: 1, filter: "blur(0px)", scale: 1 }}
+                      exit={{ opacity: 0, filter: "blur(0px)", scale: 0 }}
+                      transition={{ duration: 0.2 }}
+                    >
+                      <FaRegMoon />
+                    </motion.div>
+                  ) : (
+                    <motion.div
+                      key="sun"
+                      // initial={{ opacity: 1, filter: "blur(0px)", scale: 0.8 }}
+                      // animate={{ opacity: 1, filter: "blur(0px)", scale: 1 }}
+                      exit={{ opacity: 0, filter: "blur(0px)", scale: 0 }}
+                      transition={{ duration: 0.2 }}
+                    >
+                      <GoSun />
+                    </motion.div>
+                  )}
+                </AnimatePresence>
               </button>
             </TooltipTrigger>
             <TooltipContent className="z-1000">
-              <p className={`font-semibold ${hanken.className}`}>
-                Toggle Mode
-              </p>
+              <p className={`font-semibold ${hanken.className}`}>Toggle Mode</p>
             </TooltipContent>
           </Tooltip>
         </div>
