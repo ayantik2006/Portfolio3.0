0d8192c02dd0d9ac17869e353f5beba5f5c5ebe1
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2026-06-01 13:43:15 +0530

    ui changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,6 +25,7 @@ function Navbar() {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
 
